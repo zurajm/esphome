76b530857e45c8a612dad89c3a1ff111082bc884
zurajm <miha@zuraj.com>
2023-05-16 17:19:10 +0200

    Minor changes

--- esphome/components/as3935/as3935.cpp
+++ esphome/components/as3935/as3935.cpp
@@ -258,7 +258,8 @@ uint8_t AS3935Component::read_div_ratio() {
 
 uint8_t AS3935Component::read_capacitance() {
   ESP_LOGV(TAG, "Calling read_capacitance");
-  return uint8_t reg_val = this->read_register_(FREQ_DISP_IRQ, CAP_MASK) * 8; // Multiplied by 8pF
+  uint8_t reg_val = this->read_register_(FREQ_DISP_IRQ, CAP_MASK) * 8; // Multiplied by 8pF
+  return reg_val
 }
 
 // REG0x08, bits [5,6,7], manufacturer default: 0.
